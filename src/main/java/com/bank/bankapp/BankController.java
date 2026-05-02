448b928bd58be9cf26dfbd82bee3fb764816ef6e
nagendraveer <nagendravernekar06@gmail.com>
2026-05-02 13:28:21 +0530

    initial commit

---
+++ src/main/java/com/bank/bankapp/BankController.java
@@ -0,0 +1,17 @@
+package com.bank.bankapp;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BankController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bank App is Running!";
+    }
+
+    @GetMapping("/balance")
+    public String balance() {
+        return "Your balance is ₹10,000";
+    }
+}
